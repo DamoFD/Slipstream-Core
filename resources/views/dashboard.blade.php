2ceeced190765cb1dc6b54d182aff21674de5718
CT <chris@toxz.nl>
2022-11-05 17:08:26 +0100

    add logo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     <div class="flex flex-col basis-full mb-10"> <!-- content area -->
 
         <header class="flex h-20 flex-row justify-between mt-5 mx-8"><!-- Header -->
-            <div class="self-center"><img src="media/logo.png" alt=""></div>
+            <div class="self-center"><img src="img/logo.png" alt=""></div>
             <nav class="basis-1/6 bg-background-secondary rounded-md self-center text-base text-gray-200 py-2 px-4">
                 <ul class="flex justify-around gap-8 sm:gap-2">
                     <li class="transition-all cursor-pointer hover:text-brand-primary-500">Dashboard </li>
