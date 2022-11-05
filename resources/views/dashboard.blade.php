cdb2127e31f3fded8ee5cafcca9be89804e65495
CT <chris@toxz.nl>
2022-11-05 17:03:19 +0100

    remove sidebar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,43 +2,14 @@
     {{-- Dev --}}
     <div class="absolute bottom-0 right-0 w-10 h-10 2xl:bg-red-500 xl:bg-orange-500 lg:bg-yellow-500 md:bg-green-500 sm:bg-blue-500 "></div>
     {{-- Dev --}}
-    <div class="flex justify-center w-auto h-screen bg-background-secondary
-            transition-all duration-300"> <!-- Sidebar -->
 
-        <ul class="flex flex-col h-1/3 mt-32 text-white font-bold">
-            <li class="bg-background-surface min-w-[3rem] h-12 mb-1 mx-2 rounded-md flex justify-center items-center cursor-pointer whitespace-nowrap
-                    transition-all duration-300
-                    hover:bg-brand-primary-500 hover:after:content-['acing_day'] hover:px-2">
-                <span>R</span>
-            </li>
-
-            <li class="bg-background-surface min-w-[3rem] h-12 mb-1 mx-2 rounded-md flex justify-center items-center cursor-pointer whitespace-nowrap
-                    transition-all duration-300
-                    hover:bg-brand-primary-500 hover:after:content-['lightsim'] hover:px-2">
-                <span>F</span>
-            </li>
-
-            <li class="bg-background-surface min-w-[3rem] h-12 mb-1 mx-2 rounded-md flex justify-center items-center cursor-pointer whitespace-nowrap
-                    transition-all duration-300
-                    hover:bg-brand-primary-500 hover:after:content-['ood_contest'] hover:px-2">
-                <span>F</span>
-            </li>
-
-            <li class="bg-background-surface min-w-[3rem] h-12 mb-1 mx-2 rounded-md flex justify-center items-center cursor-pointer whitespace-nowrap
-                    transition-all duration-300
-                    hover:bg-brand-primary-500 hover:after:content-['ungee_jumping'] hover:px-2">
-                <span>B</span>
-            </li>
-
-        </ul>
-    </div>
     <div class="flex flex-col basis-full mb-10"> <!-- content area -->
 
         <header class="flex h-20 flex-row justify-between mt-5 mx-8"><!-- Header -->
             <div class="self-center"><img src="media/logo.png" alt=""></div>
             <nav class="basis-1/6 bg-background-secondary rounded-md self-center text-base text-gray-200 py-2 px-4">
                 <ul class="flex justify-around gap-8 sm:gap-2">
-                    <li class="transition-all cursor-pointer hover:text-brand-primary-500">Dashboard</li>
+                    <li class="transition-all cursor-pointer hover:text-brand-primary-500">Dashboard </li>
                     <li class="transition-all cursor-pointer hover:text-brand-primary-500">Settings</li>
                     <li class="transition-all cursor-pointer hover:text-brand-primary-500">Logout</li>
                     <button onclick="Livewire.emit('openModal', 'dev.modal')">Modal</button>
