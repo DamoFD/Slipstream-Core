02fa6e6c48c148fca44aabf3a4b8e02b5040b98b
RbDev <randell@live.nl>
2022-11-10 00:04:56 +0100

    Progres on create modal upload form

--- resources/views/livewire/tag/create.blade.php
+++ resources/views/livewire/tag/create.blade.php
@@ -3,13 +3,12 @@
      x-on:livewire-upload-finish="isUploading = false, isFinished = true"
      x-on:livewire-upload-error="isUploading = false"
      x-on:livewire-upload-progress="progress = $event.detail.progress"outline-opa
-     class="upload-area bg-gray-900 text-opacity-100 text-gray-900 px-4 py-2 cursor-pointer"
-     onclick="document.querySelector('input[type=\'file\']').click();">
+     class="upload-area bg-gray-900 text-opacity-100 text-gray-900 px-4 py-2 cursor-pointer">
     <div class="flex h-72">
         <div class="flex justify-center items-center w-full">
                 <!-- Input -->
                 <div x-show="!file">
-                    <span class="flex justify-center mb-4"><box-icon name='download' color="white" animation="flashing-hover" size="lg"></box-icon></span>
+                    <span class="flex justify-center mb-4" onclick="document.querySelector('input[type=\'file\']').click();"><box-icon name='download' color="white" animation="flashing-hover" size="lg"></box-icon></span>
                     <input id="file" hidden type="file" wire:model="file" x-ref="file" x-on:change="file = $refs.file.files[0].name">
                     <p id="dropmessage" ><span class="font-bold">Choose a file</span> or drag it here.</p>
                 </div>
@@ -20,10 +19,18 @@
                 <div x-show="file">
 
                     <!-- Form -->
-                    <p class="text-center align-middle">
-                        <input type="text" x-bind:placeholder="file" wire:model="title" id="title"><br>
-                        <textarea placeholder="Description..." wire:model="description"></textarea>
-                    </p>
+                    <div class="grid grid-cols-2 gap-4">
+                        <div class="">
+                            <p><label for="title">Title</label></p>
+                            <p class="mb-8"><input type="text" x-bind:placeholder="file" wire:model="title" id="title" class="default-input"></p>
+                            <p><label for="description">Description</label></p>
+                            <p><textarea placeholder="Description..." wire:model="description" class="default-input"></textarea></p>
+                        </div>
+
+                        <div class="">
+                            <div class="bg-gray-500 rounded-md w-64 h-full"><p class="text-center align-middle">This is a video or thumb..</p></div>
+                        </div>
+                    </div>
 
                     <!-- Uploading -->
                     <div x-show="isUploading">
@@ -33,8 +40,12 @@
 
                     <!-- Finished uploading -->
                     <div x-show="isFinished">
-                        <button wire:click.prevent="upload">Save media!</button>.
-                        <button x-on:click.prevent="file = '', isFinished = 0" id="reset">Reset</button>
+                        <button wire:click.prevent="upload">
+                            <p>Save media</p>
+                        </button>
+                        <button x-on:click.prevent="file = '', isFinished = 0" id="reset">
+                            <p>Cancel</p>
+                        </button>
                     </div>
             </div>
         </div>
