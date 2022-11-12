da13a90db16e0ee6f71db7bcd8fbd299a80d9ce6
RbDev <randell@live.nl>
2022-11-12 23:08:38 +0100

    added buttons

--- resources/views/livewire/tag/edit.blade.php
+++ resources/views/livewire/tag/edit.blade.php
@@ -7,7 +7,7 @@
         </div>
 
             <!-- Main content -->
-        <div class="px-4 flex justify-between gap-8">
+        <div class="px-4 flex justify-between gap-12">
             <div class="flex flex-col basis-3/5"><!-- coll 1 -->
 
                 <div class="aspect-video py-8"><!-- Media frame -->
@@ -17,21 +17,21 @@
                 <div class="mb-8">
                     <p class="font-light pb-2"><label for="mediaTitle">Media Title</label></p>
                     <p><input class="input-text default-input w-full" type="text" placeholder="{{ $tag->title }} ({{ $tag->tag }})" name="mediaTitle" wire:model=""></p>
-                    <sub>The title of your media</sub>
+                    <sub class="text-neutral-400">The title of your media</sub>
                 </div>
 
-                <div class="mb-8">
+                <div class="mb-6">
                     <p class="font-light pb-2"><label for="mediaDiscription">Discription</label></p>
                     <p>
                         <textarea class="input-text default-input w-full" name="mediaDiscription" id="mediaDiscription" cols="10" rows="3" wire:model=""></textarea>
                     </p>
-                    <sub>Discribe your media</sub>
+                    <sub class="text-neutral-400">Discribe your media</sub>
                 </div>
 
             </div>
 
             <div class="flex-col w-2/5"><!-- coll 2 -->
-                <div class="mb-8">
+                <div class="mb-6">
                     <p class="font-light pb-2"><label for="mediaTitle">Access policy</label></p>
                     <p>
                         <select class="input-text default-input w-1/3 pl-2" name="mediaAccessPolicy" id="mediaAccessPolicy" wire:model="">
@@ -40,31 +40,41 @@
                             <option value="private">Private</option>
                         </select>
                     </p>
-                    <sub>Select the Access policy for your media</sub>
+                    <sub class="text-neutral-400">Select the Access policy for your media</sub>
                 </div>
-                <div class="mb-8">
+                <div class="mb-6">
                     <p class="font-light pb-2"><label for="None">None</label></p>
                     <p><input class="input-text default-input w-full" type="text" name="None" wire:model=""></p>
-                    <sub>The None of your media</sub>
+                    <sub class="text-neutral-400">The None of your media</sub>
                 </div>
-                <div class="mb-8">
+                <div class="mb-6">
                     <p class="font-light pb-2"><label for="None">None</label></p>
                     <p><input class="input-text default-input w-full" type="text" name="None" wire:model=""></p>
-                    <sub>The None of your media</sub>
+                    <sub class="text-neutral-400">The None of your media</sub>
                 </div>
-                <div class="mb-8">
+                <div class="mb-6">
                     <p class="font-light pb-2"><label for="None">None</label></p>
                     <p><input class="input-text default-input w-full" type="text" name="None" wire:model=""></p>
-                    <sub>The None of your media</sub>
+                    <sub class="text-neutral-400">The None of your media</sub>
                 </div>
             </div>
+
         </div>
 
-        <div class="px-6">
+        {{-- <div class="px-6">
             <div class="button">
                 <button type="button" wire:click.prevent="update()">SAVE CHANGES</button>
             </div>
             <box-icon color="red" name="trash" animation="tada-hover" wire:click="$emit('openModal', 'tag.delete', {{ json_encode([$tag->id]) }})"></box-icon>
+        </div> --}}
+
+        <div class="mx-4">
+            <button wire:click.prevent="update()" class="btn btn-primary">
+                <p>Save changes</p>
+            </button>
+            <button wire:click="$emit('openModal', 'tag.delete', {{ json_encode([$tag->id]) }})" class="btn btn-alert">
+                <p>Delete</p>
+            </button>
         </div>
 
     </div>
